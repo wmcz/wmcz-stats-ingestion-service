504b9f45e456069a3c72221d2881f3ee44f9d5b2
Martin Urbanec <martin.urbanec@wikimedia.cz>
2021-04-09 10:15:46 +0000

    Add course_users

---
+++ course_users/load_druid.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+# cd to the directory where this script is
+scriptdir="`dirname \"$0\"`"
+cd $scriptdir
+
+curl -X 'POST' -H 'Content-Type:application/json' -d @druid_spec.json http://wmcz-stats-druid01:8081/druid/indexer/v1/task
