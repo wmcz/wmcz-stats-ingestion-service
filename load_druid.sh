184c0d87dfec45fb7ebdfdfad0f14f385f66ff4f
Martin Urbanec <martin.urbanec@wikimedia.cz>
2021-06-15 18:36:17 +0000

    load_druid: Actually load passed

--- load_druid.sh
+++ load_druid.sh
@@ -12,6 +12,4 @@ fi
 # cd to the directory where this script is
 scriptdir="`dirname \"$0\"`"
 cd $scriptdir
-
-curl -X 'POST' -H 'Content-Type:application/json' -d @druid_spec.json http://wmcz-stats-druid01:8081/druid/indexer/v1/task
-echo
+curl -X 'POST' -H 'Content-Type:application/json' -d @"$FILE" http://wmcz-stats-druid01:8081/druid/indexer/v1/task
